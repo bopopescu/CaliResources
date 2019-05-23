691df276f457a475cbc44ec5d2d54ad8d120a826
Eduardo Torres <etedoble6cinco@gmail.com>
2019-05-23 13:09:47 -0700

    fix

--- workpick/urls.py
+++ workpick/urls.py
@@ -6,19 +6,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
-    path(r'^register/',user_views.register,name='register'),
-    path(r'^login/', auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
-    path(r'^profile/' ,user_views.profile ,name='profile'),
-    path(r'^logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+    path('admin/', admin.site.urls),
+    path('register/',user_views.register,name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('profile/' ,user_views.profile ,name='profile'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
     path('',include('blog.urls')),
-    path(r'^password-reset/',
+    path('password-reset/',
     auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),name='password_reset'),
-     path(r'^password-reset-done/',
+     path('password-reset-done/',
     auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),name='password_reset_done'),
-      path(r'^password-reset-confirm/<uidb64>/<token>/',
+      path('password-reset-confirm/<uidb64>/<token>/',
     auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),name='password_reset_confirm'),
-     path(r'^password-reset-complete/',
+     path('password-reset-complete/',
     auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),name='password_reset_complete'),
  
     
