740f02da586c99623ebfd43e6267e393568bd54c
Eduardo Torres <etedoble6cinco@gmail.com>
2019-05-23 01:52:05 -0700

    fix

--- workpick/urls.py
+++ workpick/urls.py
@@ -1,7 +1,6 @@
-from django.conf.urls.defaults import *
+from django.urls import path, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path, include
 from users import views as user_views
 from django.conf import settings
 from django.conf.urls.static import static
