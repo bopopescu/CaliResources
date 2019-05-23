ed48cd945f3a1d7caa22d116b3220ffda362f0e5
Eduardo Torres <etedoble6cinco@gmail.com>
2019-05-23 02:03:26 -0700

    fix

--- workpick/urls.py
+++ workpick/urls.py
@@ -1,4 +1,5 @@
-from django.urls import include, path
+from django.urls import include
+from django.urls import path
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from users import views as user_views
