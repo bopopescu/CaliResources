7b99ab23e3a1c246a42ae46b8457b2b187bc8d7a
Eduardo Torres <etedoble6cinco@gmail.com>
2019-06-03 19:09:46 -0700

    fix

--- users/models.py
+++ users/models.py
@@ -15,11 +15,11 @@ class Disability (models.Model):
         return self.disability
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    firstname = models.CharField(max_length=255 ,default='')
-    lastname = models.CharField(max_length=255 , default='')
+    firstname = models.CharField(max_length=255, default='nombre')
+    lastname = models.CharField(max_length=255, default='apellido')
     phone = PhoneField(blank=True, help_text='Numero de Celular')
-    country = CountryField(default='Mexico')
-    bornDate = models.DateField('Fecha de nacimiento',blank=False, null = False ,default=datetime.date.today)
+    country = CountryField(default='mexico')
+    bornDate = models.DateField('Fecha de nacimiento',blank=False,default=datetime.date.today)
     image  = models.ImageField(default='default.png',upload_to='profile_pics')
     disability = models.ManyToManyField(Disability)
     language = models.ManyToManyField(Language)
